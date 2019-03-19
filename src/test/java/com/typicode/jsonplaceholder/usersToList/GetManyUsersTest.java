b027307b4887b88223a7eeb5de959d319cd3dc57
Miczura <m.gieleta@gmail.com>
2019-03-19 17:12:44 +0100

    new test

--- src/test/java/com/typicode/jsonplaceholder/usersToList/GetManyUsersTest.java
+++ src/test/java/com/typicode/jsonplaceholder/usersToList/GetManyUsersTest.java
@@ -1,5 +1,6 @@
 package com.typicode.jsonplaceholder.usersToList;
 
+import com.typicode.jsonplaceholder.users.Geo;
 import com.typicode.jsonplaceholder.utils.Endpoint;
 import com.typicode.jsonplaceholder.utils.StatusCodes;
 import com.typicode.jsonplaceholder.users.Users;
@@ -9,6 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.typicode.jsonplaceholder.utils.DeserializerForListsGeneric.deserializeToList;
 import static io.restassured.RestAssured.given;
@@ -26,11 +28,21 @@ public class GetManyUsersTest {
         Assert.assertEquals(10,counUsersInResponse());
 
     }
-
-
-    private int counUsersInResponse(){
-        List<Users> usersInResponseList = deserializeToList(response,Users.class);
-        return usersInResponseList.size();
+    @Test
+    public void checkCountOfLattitudeAttibutes(){
+        Assert.assertEquals(10,countLattitudeAttributes());
     }
 
+    private List<Users> getObjectsFromResponse(){
+        List<Users> usersInResponseList = deserializeToList(response,Users.class);
+        return usersInResponseList;
+    }
+    private int counUsersInResponse(){
+
+        return getObjectsFromResponse().size();
+    }
+    private int countLattitudeAttributes(){
+        List<String> lattitudeList = getObjectsFromResponse().stream().map(lattitude->lattitude.getAddress().getGeo().getLat()).collect(Collectors.toList());
+        return lattitudeList.size();
+    }
 }
