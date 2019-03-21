c6a20f7b5b9c7fafab2f435c3d45fdc0f2eec289
Miczura <m.gieleta@gmail.com>
2019-03-21 16:59:30 +0100

    initial commit for POST test

---
+++ src/main/java/com/typicode/jsonplaceholder/posts/Employee.java
@@ -0,0 +1,25 @@
+package com.typicode.jsonplaceholder.posts;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private int userId;
+    private int id;
+    public Employee(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
