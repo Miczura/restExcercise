a2f3c0f17e181b13d9d299712a91d309da7020c3
Miczura <m.gieleta@gmail.com>
2019-03-22 16:43:45 +0100

    refactored deserialiser to one object of any type. Added generic deserialiser method

---
+++ src/main/java/com/typicode/jsonplaceholder/utils/DeserialiserForSingleObjectGeneric.java
@@ -0,0 +1,14 @@
+package com.typicode.jsonplaceholder.utils;
+
+import io.restassured.response.Response;
+
+
+public class DeserialiserForSingleObjectGeneric {
+
+//    public static Users deserialiseToUsersObject(Response response){
+//        return response.body().jsonPath().getObject("", Users.class);
+//    }
+    public static <T> T deserialiseToAnyObject(Response response, Class<T> Name){
+        return response.body().jsonPath().getObject("", Name);
+    }
+}
