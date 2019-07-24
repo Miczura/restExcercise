bb344940f703607b263b8e02f34dbb9f628162fa
Miczura <m.gieleta@gmail.com>
2019-07-24 17:31:43 +0200

    Added new endpoint

--- src/main/java/utils/Endpoint.java
+++ src/main/java/utils/Endpoint.java
@@ -5,4 +5,5 @@ public class Endpoint {
     public static final String USERS_1_ENDPOINT = "https://jsonplaceholder.typicode.com/users/1";
     public static final String USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users/";
     public static final String POSTS_ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
+    public static final String IN_REGRES_GET_USERS_ENDPOINT="https://reqres.in/api/users?page=2";
 }
