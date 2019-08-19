2997ba22b4fb4473878259b85b26a8a01575bbb0
Miczura <m.gieleta@gmail.com>
2019-08-19 17:52:32 +0200

    Added few new tests

--- src/test/java/com/typicode/jsonplaceholder/users/GetOneUserTest.java
+++ src/test/java/com/typicode/jsonplaceholder/users/GetOneUserTest.java
@@ -1,11 +1,15 @@
 package com.typicode.jsonplaceholder.users;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import utils.Endpoint;
 import utils.StatusCodes;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static utils.DeserialiserForSingleObjectGeneric.deserialiseToAnyObject;
 import static io.restassured.RestAssured.*;
@@ -14,30 +18,59 @@ import static org.hamcrest.Matchers.*;
 
 public class GetOneUserTest {
     private static Response response;
+    private static Users deserialisedUsersResponseObject;
     @BeforeClass
     public static void getUsersEndpoint(){
         response = given().expect().statusCode(StatusCodes.SUCCESS)
                 .when().get(Endpoint.USERS_1_ENDPOINT);
+        deserialisedUsersResponseObject = deserialiseToAnyObject(response,Users.class);
     }
     @Test
-    public void getUsersCheckSizeOfJsonResponseBody(){
+    public void CheckSizeOfJsonResponseBody(){
         response.then().assertThat().body("size()",is(8));
     }
 
     @Test
-    public void getUsersCheckSizeOfAddressAttributeInResponseBody(){
+    public void CheckSizeOfAddressAttributeInResponseBody(){
         response.then().assertThat().body("address.size()",is(5));
     }
 
     @Test
-    public void getUsersCheckLongnitudeInResponeBody(){
+    public void CheckLongnitudeInResponeBody(){
         response.then().assertThat().body("address.geo.lng",equalTo("81.1496"));
         System.out.println("Test Passed");
     }
     @Test
-    public void getUsersDeserialisationToObjectTest(){
-        Users deserialisedUsersResponseObject = deserialiseToAnyObject(response,Users.class);
-        Assert.assertTrue("getBs do not contain \"e-market\"",deserialisedUsersResponseObject.getCompany().getBs().contains("e-markets"));
+    public void checkgetBsAttributeValuetTest(){
+
+        Assert.assertTrue("getBs attribute do not contain \"e-market\"",deserialisedUsersResponseObject.getCompany().getBs().contains("e-markets"));
         assertThat(deserialisedUsersResponseObject.getCompany().getBs(),is(equalTo("harness real-time e-markets")));
+    }
+
+    @Test
+    public void checkUserIdObjetMapperTest(){
+        assertThat(deserializeResponseToObject(response).getId(),is(1));
+    }
+
+    @Test
+    public void checkUserNameObjetMapperTest(){
+        assertThat(deserializeResponseToObject(response).getName(),is(equalTo("Leanne Graham")));
+    }
+
+    @Test
+    public void checkNameObjetMapperTest(){
+        assertThat(deserializeResponseToObject(response).getUsername(),is(equalTo("Bret")));
+    }
+
+    private Users deserializeResponseToObject(Response response)  {
+        ObjectMapper mapper = new ObjectMapper();
+        String body = response.getBody().asString();
+        Users userObject=null;
+        try {
+            userObject = mapper.readValue(body, Users.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userObject;
     }
 }
