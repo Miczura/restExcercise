22143327df690f712706ae814481af70005b1b2c
Miczura <m.gieleta@gmail.com>
2020-04-16 08:01:06 +0200

    New property in json

---
+++ src/main/java/in/regres/listUsers/Ad.java
@@ -0,0 +1,23 @@
+package in.regres.listUsers;
+
+public class Ad {
+    private String company;
+    private String url;
+    private String text;
+
+    public Ad(){
+
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
